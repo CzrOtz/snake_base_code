8578d72ec947dcc099617eec94e4a263196b4781
CzrOtz <100041702+CzrOtz@users.noreply.github.com>
2023-03-09 14:25:30 -0500

    Update game.py

--- game.py
+++ game.py
@@ -1,13 +1,14 @@
 # Import the Pygame library
 from re import L
 import pygame
+import random
 
 # Initialize Pygame
 pygame.init()
 
 # Set up the game window
-WINDOW_WIDTH = 640
-WINDOW_HEIGHT = 480
+WINDOW_WIDTH = 840
+WINDOW_HEIGHT = 680
 game_window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 
 # Set up the game grid
@@ -21,29 +22,68 @@ snake_direction = 'down'  # The direction the snake is moving in
 
 head_x, head_y = snake_blocks[0] #this is called touple unpacking
 
+r1_x = random.randint(0, 40)
+r1_y = random.randint(0, 40)
+r2_x = random.randint(0, 40)
+r2_y = random.randint(0, 40)
+
+snake_food = [(r1_x, r1_y), (r2_x, r2_y)]
+
+
+
+
+clock = pygame.time.Clock()
+
 # Main game loop
 running = True
+
+once = True
+
 while running:
-    # Handle events
-   
+    
+     # Update the position of the food only if the snake has eaten it
+    if (head_x, head_y) in snake_food:
+        snake_food.remove((head_x, head_y))
+        r1_x = random.randint(0, 40)
+        r1_y = random.randint(0, 40)
+        r2_x = random.randint(0, 40)
+        r2_y = random.randint(0, 40)
+
+    
+    snake_food = [(r1_x, r1_y), (r2_x, r2_y)]
+
+    
+    # Store the position of the food in a list
+    
+
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
     
         if event.type == pygame.KEYDOWN:
-            if event.type == pygame.K_UP:
-                head_y -= 1
             
-            if event.type == pygame.K_DOWN:
-                head_y += 1
+            if event.key == pygame.K_UP:
+                snake_direction = 'up'
             
-            if event.type == pygame.K_RIGHT:
-                head_x += 1
+            if event.key == pygame.K_DOWN:
+                snake_direction = 'down'
             
-            if event.type == pygame.K_LEFT:
-                head_x -= 1
+            if event.key == pygame.K_RIGHT:
+                snake_direction = 'right'
+            
+            if event.key == pygame.K_LEFT:
+                snake_direction = 'left'
 
+
+    if snake_direction == 'up':
+        head_y -= 1
+    elif snake_direction == 'down':
+        head_y += 1
+    elif snake_direction == 'right':
+        head_x += 1
+    elif snake_direction == 'left':
+        head_x -= 1
     
     
     
@@ -52,15 +92,24 @@ while running:
     #this is for boundaries
     if head_x < 0 or head_x >= GRID_WIDTH or head_y < 0 or head_y >= GRID_HEIGHT:
         running = False
+
     
     # Check for collision with snake body
+    #checking to see if the tuple is in the second to i'th element of the array
+    #what does it mean if the head is somewhere else other than the front? collision
     if (head_x, head_y) in snake_blocks[1:]:
+        print('triggered by me,  if (head_x, head_y) in snake_blocks[1:]: ')
         running = False
     
     # Update the snake's position
-    #if 
+    #at index 0 of the snake, you're inserting a touple contianing the next block
+    
     snake_blocks.insert(0, (head_x, head_y))
-    if (head_x, head_y) != snake_blocks[-1]:
+    
+    
+
+
+    if (head_x, head_y) != snake_blocks[-2]:
         snake_blocks.pop()
     
     # Clear the screen
@@ -72,15 +121,35 @@ while running:
     for y in range(GRID_HEIGHT):
         pygame.draw.line(game_window, (255, 255, 255), (0, y * GRID_SIZE), (WINDOW_WIDTH, y * GRID_SIZE))
     
-    # Draw the snake
+    # draws the snake
+    #by making a loop that draws a rect for 
+
     for block in snake_blocks:
+        #the position of the blocks are miltiples of the grid so it gives it the 
+        #illusion of being whithin the grid
+        #in reality whats happening is that the blocks are fitting within the grid lines
         block_rect = pygame.Rect(block[0] * GRID_SIZE, block[1] * GRID_SIZE, GRID_SIZE, GRID_SIZE)
         pygame.draw.rect(game_window, (0, 255, 0), block_rect)
+
+        print(snake_blocks, ' snake block')
+
+    #display the snake food
+    # pygame.Rect(x, y, width, height)
+    for food in snake_food:
+        food_rect = pygame.Rect(food[0], food[1], GRID_SIZE, GRID_SIZE)
+        pygame.draw.rect(game_window, (255,0,0), food_rect)
+        print(snake_food)
+
+        if (head_x, head_y) == food:
+            snake_blocks.append(snake_blocks[-1])  # Add a new block to the snake
+            snake_food.remove(food)  # Remove the food that was eaten
+
+    
     
     # Update the screen
     pygame.display.update()
     
     # Set the game's frame rate
-    FPS = 60
-    clock = pygame.time.Clock()
+    FPS = 10
+
     clock.tick(FPS)
